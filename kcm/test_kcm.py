6111a2d6108ce69e97b1f12d86c8e0389f97914d
voidful <voidful.stack@gmail.com>
2020-07-09 14:19:07 +0800

    fix kcm test case sort order add test case description

--- kcm/test_kcm.py
+++ kcm/test_kcm.py
@@ -31,17 +31,19 @@ class TestKCM(unittest.TestCase):
             self.assertEqual(len(sim(word)), 10)
 
     def test_sort(self):
+        # result should be sort by frequency in descending order
         for word in self.test_word_list:
-            self.assertTrue(all(sim(word)[i][1] <= sim(word)[i + 1][1] for i in range(len(sim(word)) - 1)))
+            self.assertTrue(all(sim(word)[i][1] >= sim(word)[i + 1][1] for i in range(len(sim(word)) - 1)))
 
     def test_length(self):
+        # one word result should be removed
         for word in self.test_word_list:
             self.assertTrue(len(sim(word)[0]) > 1)
 
     def test_example(self):
         example_list = ['政府', '核四', '政治', '事件', '盧碧', '周子瑜', '洪素珠', '林義雄', '日本', '林冠']
         overlap = set([result[0] for result in sim('台灣')]) & set(example_list)
-        self.assertTrue(len(overlap)/10 > 0.7)
+        self.assertTrue(len(overlap) / 10 > 0.7)
 
 
 if __name__ == '__main__':
