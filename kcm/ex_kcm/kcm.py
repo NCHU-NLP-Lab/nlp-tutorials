515871e2045c9f2c4459a10238d76d2ca3fd52a6
Chen-Hua Huang <88367016+huang624@users.noreply.github.com>
2023-08-09 13:24:41 +0800

    Update kcm.py

--- kcm/ex_kcm/kcm.py
+++ kcm/ex_kcm/kcm.py
@@ -8,8 +8,15 @@ def sim(input):
     ----------
     input : string
         only keywords on kcm dict will return result, otherwise return empty list
-    Returns
-    -------
-    value : list of dict, result keywords as keys, and its frequency will be value.
+        
+    output : two-dimensional list
+        The output is a two-dimensional list.
+        The output contains 10 elements.
+        Each element is a list with two items:
+            The first item is related keyword and the second item is frequency.
+
+        Example:
+        [ [“高雄市”, 39], [“國民黨”, 39], [“候選人”, 24],……. ]
+
     """
     pass
